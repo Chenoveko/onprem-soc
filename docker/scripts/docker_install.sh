c305f682aa3ca446f75960a5a9f55aa5d2a2190d
Chenoveko <jastorgaybarra@gmail.com>
2025-11-28 14:17:42 +0100

    28/11/2025

--- docker/scripts/docker_install.sh
+++ docker/scripts/docker_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #==========Install Docker on Ubuntu using the apt repository==========#
-#1º) Set up Docker's apt repository.
+# 1º) Set up Docker's apt repository.
 # Add Docker's official GPG key:
 sudo apt update
 sudo apt install ca-certificates curl
@@ -19,5 +19,18 @@ EOF
 
 sudo apt update
 
-#2º) Install the Docker packages.
+# 2º) Install the Docker packages.
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verify that Docker is running -> sudo systemctl status docker
+# Verify that the installation is successful -> sudo docker run hello-world
+
+# 3º) Linux post-installation steps for Docker Engine
+# Create the docker group
+sudo groupadd docker
+# Add your user to the docker group
+sudo usermod -aG docker $USER
+# Activate the changes to groups
+newgrp docker   
+
+# Verify that you can run docker commands without sudo -> docker run hello-world
