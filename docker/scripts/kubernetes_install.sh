eb7f710861fcb63cbd50326189998e688b05dbbe
Chenoveko <106373867+Chenoveko@users.noreply.github.com>
2026-01-23 17:26:00 +0100

    Update kubernetes_install.sh

--- docker/scripts/kubernetes_install.sh
+++ docker/scripts/kubernetes_install.sh
@@ -2,4 +2,4 @@
 sudo apt update && sudo apt upgrade -y && sudo apt install containerd
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo >> /home/$USER/.bashrc
-brew install gcc nerdctl
+brew install gcc nerdctl k9s
